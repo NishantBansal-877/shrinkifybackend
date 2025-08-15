37cbe4dfb8d57d61a0a5dc395c40733aa792cb03
NishantBansal-877 <nishant87706@gmail.com>
2025-08-15 22:43:27 +0530

    Update refreshAccess.js

--- src/utils/refreshAccess.js
+++ src/utils/refreshAccess.js
@@ -7,6 +7,6 @@ console.log(payload);
     return res.status(404).json({ message: "not valid refreshtoken" });
   }
   payload._id = payload.sub;
-  await createAccessToken(payload, res);
+  createAccessToken(payload, res);
   res.status(200).json({ message: "refresh access done", fullName: payload.fullName, email: payload.email });
 }
