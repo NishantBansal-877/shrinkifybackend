3277986b48c2fc0be54be18d32381f849ca553df
NishantBansal-877 <nishant87706@gmail.com>
2025-08-15 21:12:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "500mb" }));
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
